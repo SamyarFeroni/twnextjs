18d62634e954a1ffbdbd83ade7a69f0cceb11081
SAMYAR_SR <s.feroni@domil.io>
2024-10-08 10:42:40 +0330

    add new

--- src/app/components/HeaderMain.js
+++ src/app/components/HeaderMain.js
@@ -14,36 +14,19 @@ function HeaderMain() {
           style={{ right: "170px" }}
         >
           <div className="mb-14 text-right">
-            <ShowImage
-              alt={"Banimode Logo"}
-              itemLinks={ImageLink.BanimodeLogo}
-              widths={360}
-              heights={60}
-            />
+            <ShowImage alt={"Banimode Logo"} itemLinks={ImageLink.BanimodeLogo} widths={360} heights={60}/>
           </div>
           <div
             className="bg-gradient-to-l from-green-500 to-green-700 p-4 rounded-lg shadow-md mb-4 cursor-pointer relative flex justify-center"
             style={{ width: "240px", height: "52px" }}
           >
-            <ShowImage
-              alt={"Bazar Logo"}
-              itemLinks={ImageLink.BazarLogo}
-              widths={50}
-              heights={60}
-            />
+            <ShowImage alt={"Bazar Logo"} itemLinks={ImageLink.BazarLogo} widths={50} heights={60}/>
             <div className="text-white w-fit ml-2">دریافت اپلیکیشن از</div>
           </div>
 
           <div
-            className="bg-gray-50 border border-black p-4 rounded-lg shadow-md mb-4 cursor-pointer relative flex justify-center"
-            style={{ width: "240px", height: "52px" }}
-          >
-            <ShowImage
-              alt={"sibapp Logo"}
-              itemLinks={ImageLink.sibappLogo}
-              widths={56}
-              heights={60}
-            />
+            className="bg-gray-50 border border-black p-4 rounded-lg shadow-md mb-4 cursor-pointer relative flex justify-center" style={{ width: "240px", height: "52px" }}>
+            <ShowImage alt={"sibapp Logo"} itemLinks={ImageLink.sibappLogo} widths={56} heights={60}/>
             <div className="text-gray-950 ml-2 w-fit">دریافت اپلیکیشن از</div>
           </div>
 
@@ -54,39 +37,14 @@ function HeaderMain() {
             <div style={{ color: "#00bf6f" }} className="w-fit">
               دانلود اپلیکیشن
             </div>
-            <ShowImage
-              alt={"DWN Logo"}
-              itemLinks={ImageLink.DWNLogo}
-              heights={24}
-              widths={24}
-            />
+            <ShowImage  alt={"DWN Logo"}  itemLinks={ImageLink.DWNLogo}  heights={24}  widths={24}/>
           </div>
 
           <div className="flex justify-between mt-4">
-            <ShowImage
-              alt="twitter"
-              itemLinks={ImageLink.twitter}
-              widths={20}
-              heights={20}
-            />
-            <ShowImage
-              alt="aparat"
-              itemLinks={ImageLink.aparat}
-              widths={20}
-              heights={20}
-            />
-            <ShowImage
-              alt="telegram"
-              itemLinks={ImageLink.telegram}
-              widths={20}
-              heights={20}
-            />
-            <ShowImage
-              alt="instagram"
-              itemLinks={ImageLink.instagram}
-              widths={20}
-              heights={20}
-            />
+            <ShowImage alt="twitter"  itemLinks={ImageLink.twitter}  widths={20} heights={20}/>
+            <ShowImage alt="aparat"  itemLinks={ImageLink.aparat}  widths={20} heights={20}/>
+            <ShowImage alt="telegram"  itemLinks={ImageLink.telegram}  widths={20} heights={20}/>
+            <ShowImage alt="instagram" itemLinks={ImageLink.instagram} widths={20} heights={20}/>
           </div>
         </div>
 
@@ -110,38 +68,25 @@ function HeaderMain() {
 
           <div className="flex items-center mb-7 text-sm">
             <span className="text-gray-400 mr-2">ایمیل: </span>
-            <a
-              href="mailto:customer@banimode.com"
-              className="hover-colorNEW text-gray-950 w-fit"
-            >
+            <a href="mailto:customer@banimode.com" className="hover-colorNEW text-gray-950 w-fit">
               customer@banimode.com
             </a>
           </div>
 
           <div>
-            <ShowImage
-              alt={"Bani Mag"}
-              itemLinks={ImageLink.BaniMag}
-              heights={150}
-              widths={150}
+            <ShowImage alt={"Bani Mag"} itemLinks={ImageLink.BaniMag} heights={150} widths={150}
             />
           </div>
         </div>
 
         {/* The Third column on left side  */}
-        <Column
-          title={"اطلاعات بانی مد"}
-          items={LinkTexts.BanimodeInformation}
-        />
+        <Column title={"اطلاعات بانی مد"} items={LinkTexts.BanimodeInformation}/>
 
         {/* The secend column on right side */}
         <Column title={"خدمات مشتریان"} items={LinkTexts.CustomerService} />
 
         {/* The First column on right side  */}
-        <Column
-          title={"خرید از بانی مد"}
-          items={LinkTexts.BuyingFromBanimode}
-        />
+        <Column title={"خرید از بانی مد"} items={LinkTexts.BuyingFromBanimode}/>
       </div>
     </div>
   );
